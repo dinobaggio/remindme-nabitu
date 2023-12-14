458b3dc545508a095243406402e2ff66090a2e11
dinobaggio <dino.baggio007@gmail.com>
2023-12-14 13:33:23 +0700

    Feat: - adding endpoint update reminder - adding endpoint view reminder - adding endpoint delete reminder

--- src/app/Http/Controllers/RemindersController.php
+++ src/app/Http/Controllers/RemindersController.php
@@ -2,27 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Reminder;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class RemindersController extends Controller
 {
+    public function index(Request $request)
+    {
+        $user = Auth::user(); // Anda mungkin perlu menyesuaikan ini sesuai dengan model pengguna yang Anda gunakan
+
+        $limit = $request->input('limit', 10);
+
+        $reminders = Reminder::orderBy('remind_at', 'asc')
+            ->take($limit)
+            ->get();
+
+        return response()->json([
+            'ok' => true,
+            'data' => [
+                'reminders' => $reminders,
+                'limit' => $limit,
+            ]
+        ]);
+    }
+
     public function create(Request $request)
     {
-        // Validasi input
         $validator = Validator::make($request->all(), [
             'title' => 'required',
             'remind_at' => 'required|date',
             'event_at' => 'required|date',
         ]);
 
-        // Jika validasi gagal
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 422);
+            return response()->json([
+                'ok' => false,
+                'err' => 'ERR_BAD_REQUEST',
+                'msg' => $validator->errors()
+            ], Response::HTTP_BAD_REQUEST);
         }
 
-        // Buat reminder
         $reminder = Reminder::create([
             'title' => $request->input('title'),
             'description' => $request->input('description'),
@@ -30,7 +53,103 @@ class RemindersController extends Controller
             'event_at' => $request->input('event_at'),
         ]);
 
-        // Beri respons sukses
-        return response()->json(['message' => 'Reminder created successfully', 'data' => $reminder], 201);
+        return response()->json([
+            'message' => 'Reminder created successfully', 
+            'data' => [
+                'title' => $reminder->title,
+                'description' => $reminder->description,
+                'remind_at' => strtotime($reminder->remind_at),
+                'event_at' => strtotime($reminder->event_at),
+            ]
+        ], Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'remind_at' => 'required|date',
+            'event_at' => 'required|date',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'ok' => false,
+                'err' => 'ERR_BAD_REQUEST',
+                'msg' => $validator->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $reminder = Reminder::find($id);
+
+        if (!$reminder) {
+            return response()->json([
+                "ok" => false,
+                "err" => "ERR_NOT_FOUND",
+                "msg" => "resource is not found"
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $reminder->update([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'remind_at' => $request->input('remind_at'),
+            'event_at' => $request->input('event_at'),
+        ]);
+
+        return response()->json([
+            'message' => 'Reminder updated successfully', 
+            'data' => [
+                'title' => $reminder->title,
+                'description' => $reminder->description,
+                'remind_at' => strtotime($reminder->remind_at),
+                'event_at' => strtotime($reminder->event_at),
+            ]
+        ]);
+    }
+
+    public function view($id)
+    {
+        $reminder = Reminder::find($id);
+
+        if (!$reminder) {
+            return response()->json([
+                "ok" => false,
+                "err" => "ERR_NOT_FOUND",
+                "msg" => "resource is not found"
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        // Mengubah format tanggal menggunakan strtotime
+        $formattedRemindAt = strtotime($reminder->remind_at);
+        $formattedEventAt = strtotime($reminder->event_at);
+
+        return response()->json([
+            'ok' => true,
+            'data' => [
+                'id' => $reminder->id,
+                'title' => $reminder->title,
+                'description' => $reminder->description,
+                'remind_at' => $formattedRemindAt,
+                'event_at' => $formattedEventAt,
+            ],
+        ]);
+    }
+
+    public function delete($id)
+    {
+        $reminder = Reminder::find($id);
+
+        if (!$reminder) {
+            return response()->json([
+                "ok" => false,
+                "err" => "ERR_NOT_FOUND",
+                "msg" => "resource is not found"
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $reminder->delete();
+
+        return response()->json(['ok' => true]);
     }
 }
