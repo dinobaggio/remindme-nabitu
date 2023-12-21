314f5fdce77e0916776347d94a50ad7015eef575
dinobaggio <dino.baggio007@gmail.com>
2023-12-21 11:52:00 +0700

    chore: adding test login

---
+++ src/resources/js/tests/login.test.js
@@ -0,0 +1,27 @@
+import { mount } from "@vue/test-utils";
+import { expect, test } from 'vitest'
+import Login from "../pages/login.vue";
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+test('test login page', () => {
+  const wrapper = mount(Login)
+  expect(wrapper.get('[type="submit"]').text()).toContain('Sign in')
+  expect(wrapper.get('section').text()).toContain('Sign in to your account')
+})
+
+test('test required form login', async () => {
+  const wrapper = mount(Login)
+  wrapper.get('[name="email"]').setValue('')
+  wrapper.get('[name="password"]').setValue('')
+  wrapper.get('[type="submit"]').trigger('submit')
+
+  await sleep(1000)
+  
+  expect(wrapper.get('ul').text()).toContain('email required')
+  expect(wrapper.get('ul').text()).toContain('password required')
+})
--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    test: {
+        environment: 'happy-dom',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
