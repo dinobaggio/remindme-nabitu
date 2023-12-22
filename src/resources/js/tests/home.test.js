f89ed9b0602d786ba14fc147d4af13365b24cdf4
dinobaggio <dino.baggio007@gmail.com>
2023-12-23 05:30:15 +0700

    chore: adjust

--- src/resources/js/tests/home.test.js
+++ src/resources/js/tests/home.test.js
@@ -3,7 +3,7 @@ import { expect, test  } from 'vitest'
 import Home from '../pages/home.vue'
 import { LOCALSTORAGE_KEY } from "../libs/constants"
 
-test('test home page', async () => {
+test('test home page layout', async () => {
     localStorage.setItem(LOCALSTORAGE_KEY.ACCESS_TOKEN, 'asdf')
     localStorage.setItem(LOCALSTORAGE_KEY.REFRESH_TOKEN, 'ASDFF')
     const wrapper = mount(Home)
@@ -22,9 +22,7 @@ test('test home contain reminders', async () => {
     await wrapper.vm.$nextTick()
     wrapper.vm.loading = false
     const reminder = { id: 1, title: 'Reminder test', description: 'Description reminder' }
-    wrapper.vm.reminders = [
-        reminder
-    ]
+    wrapper.vm.reminders = [ reminder ]
     await wrapper.vm.$nextTick()
     expect(wrapper.find('h2').text()).toContain('list reminders')
     expect(wrapper.find('h5').text()).toContain(reminder.title)
