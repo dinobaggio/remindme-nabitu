9d87793e0cab30e1a3b4b6b711eefff45e6d3a5b
dinobaggio <dino.baggio007@gmail.com>
2023-12-25 18:13:03 +0700

    chore: ci sqlite test

--- src/database/migrations/2023_12_25_163725_add_column_to_table_reminders.php
+++ src/database/migrations/2023_12_25_163725_add_column_to_table_reminders.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -22,8 +23,12 @@ return new class extends Migration
      */
     public function down(): void
     {
+        if (DB::getDriverName() !== 'sqlite') {
+            Schema::table('reminders', function (Blueprint $table) {
+                $table->dropForeign(['user_id']);
+            });
+        }
         Schema::table('reminders', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
             $table->dropColumn('user_id');
         });
     }
